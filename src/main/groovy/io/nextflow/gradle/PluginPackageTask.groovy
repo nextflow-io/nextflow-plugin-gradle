040ee9af2062886ee5d4c2edf701ce3536bb856e
Tom Sellman <tom.sellman@seqera.io>
2025-02-25 10:31:31 +0000

    Check the declared plugin class is present in the source dirs
    Signed-off-by: Tom Sellman <tom.sellman@seqera.io>

--- src/main/groovy/io/nextflow/gradle/PluginPackageTask.groovy
+++ src/main/groovy/io/nextflow/gradle/PluginPackageTask.groovy
@@ -1,5 +1,8 @@
 package io.nextflow.gradle
 
+import org.apache.commons.io.FilenameUtils
+import org.gradle.api.tasks.SourceSet
+import org.gradle.api.tasks.SourceSetContainer
 import org.gradle.api.tasks.bundling.Zip
 
 /**
@@ -15,5 +18,49 @@ class PluginPackageTask extends Zip {
         into('lib') { from project.configurations.runtimeClasspath }
         preserveFileTimestamps = false
         reproducibleFileOrder = true
+    }
+
+    @Override
+    protected void copy() {
+        // first do a sanity check
+        def config = project.extensions.getByType(NextflowPluginConfig)
+        checkPluginClassIncluded(config.className)
+
+        // then call parent class to perform the task
+        super.copy()
+    }
+
+    // Scan the sources to check that the declared main plugin classes is included
+    private void checkPluginClassIncluded(String className) {
+        def sourceSets = project.extensions.getByType(SourceSetContainer)
+            .named(SourceSet.MAIN_SOURCE_SET_NAME).get()
+
+        // get a list of resources dirs to be ignored
+        def resourcesDirs = sourceSets.getResources().getSourceDirectories()
+        // get all the sources dirs (which includes resources)
+        def sources = sourceSets.getAllSource()
+
+        // try to find the plugin class in the source dirs
+        def classNameAsPath = className.replaceAll('\\.', File.separator)
+        def matched = sources.find { file ->
+            def name = FilenameUtils.removeExtension(file.absolutePath)
+            for (def dir : sources.getSourceDirectories()) {
+                if (!resourcesDirs.contains(dir) && name.startsWith(dir.absolutePath)) {
+                    name = name.substring(dir.absolutePath.length()+1)
+                    break
+                }
+            }
+            return name == classNameAsPath
+        }
+
+        if (!matched) {
+            def message = "--------------------------------------------------------------------------------\n" +
+                "Plugin class '$className' not found in source directories:\n"
+            for (def dir : sources.getSourceDirectories()) {
+                if (!resourcesDirs.contains(dir)) message += "\n- $dir"
+            }
+            message += "\n--------------------------------------------------------------------------------"
+            throw new RuntimeException(message)
+        }
     }
 }
